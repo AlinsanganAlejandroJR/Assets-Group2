83a089da056a047d1ee22fd01525ab3194c00428
Alejandro <alejandro.softvision@.com>
2022-11-14 18:12:31 +0800

    Rename the Controller package to controller

--- src/main/java/com/academy/assetsproject/controller/ProductController.java
+++ src/main/java/com/academy/assetsproject/controller/ProductController.java
@@ -1,11 +1,13 @@
-package com.academy.assetsproject.Controller;
+package com.academy.assetsproject.controller;
 
 public class ProductController {
-    //TODo
-    //TODO Autowired product service
+
+        //TODo
+        //TODO Autowired product service
    /* @GetMapping("{id}")
     public Products getProduct(@RequestParam Products products){
         System.out.println(products);
         return products;
     }*/
+
 }
