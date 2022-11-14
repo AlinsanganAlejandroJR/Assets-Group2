577f591c5b9f65877bebe12b654869868c7bc595
Alejandro <alejandro.softvision@.com>
2022-11-14 18:40:21 +0800

    Fix folders and Added VIC Code, closing the branch ThirdVersionFix

---
+++ src/main/java/com/academy/assetsproject/exception/RecordNotFoundException.java
@@ -0,0 +1,12 @@
+package com.academy.assetsproject.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class RecordNotFoundException extends Throwable {
+    private static final long serialVersionUID = 1L;
+    public RecordNotFoundException(String message) {
+        super();
+    }
+}
--- src/main/java/com/academy/assetsproject/services/ProductTypeService.java
+++ src/main/java/com/academy/assetsproject/services/ProductTypeService.java
@@ -1,4 +1,14 @@
 package com.academy.assetsproject.services;
 
+import com.academy.assetsproject.models.ProductTypes;
+import org.springframework.data.domain.Page;
+import com.academy.assetsproject.exception.RecordNotFoundException;
+import org.springframework.data.domain.Pageable;
+
 public interface ProductTypeService {
+    Page<ProductTypes> findByProductTypes(ProductTypes productTypes, Pageable pageable);
+    Page<ProductTypes> findAllProductTypes(Pageable pageable);
+    ProductTypes findProductTypesById(Long id)throws RecordNotFoundException;
+    ProductTypes updateProductTypes(ProductTypes productType, Long id)throws RecordNotFoundException;
+    void deleteProductTypes(Long id, Pageable pageable)throws RecordNotFoundException;
 }
