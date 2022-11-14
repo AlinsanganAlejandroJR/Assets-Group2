4c3d4b479f1265330ad4d2309b088895777e0f32
Alejandro <alejandro.softvision@.com>
2022-11-14 16:08:07 +0800

    First Version

---
+++ src/main/java/com/academy/assetsproject/AssetsprojectApplication.java
@@ -0,0 +1,13 @@
+package com.academy.assetsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssetsprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssetsprojectApplication.class, args);
+	}
+
+}
