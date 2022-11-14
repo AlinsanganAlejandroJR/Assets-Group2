80ff2d92d787abf9fbb6658406f51cbdb439278e
Ludivico <ludivicooguan192@gmail.com>
2022-11-14 17:10:04 +0800

    Added Product controller and repositories

---
+++ src/main/java/com/academy/assetsproject/Repository/ProductTypesRepository.java
@@ -0,0 +1,11 @@
+package com.academy.assetsproject.Repository;
+
+import com.academy.assetsproject.models.ProductTypes;
+import com.academy.assetsproject.models.Products;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface ProductTypesRepository extends PagingAndSortingRepository<ProductTypes, Long> {
+    Page<ProductTypes> findByProduct(Products products, Pageable pageable);
+}
--- src/main/java/com/academy/assetsproject/models/Products.java
+++ src/main/java/com/academy/assetsproject/models/Products.java
@@ -4,10 +4,15 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.time.LocalDate;
 
 @Entity
 public class Products {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long Id;
+    private String name;
+    private double price;
+    private LocalDate dateOfPurchase;
+
 }
