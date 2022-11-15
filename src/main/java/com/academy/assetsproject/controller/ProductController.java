2a272b5c1efdbc2947d4c8ea64849b3f77e52193
AlejandroAlinsangan <alejandro.alinsangan@softvision.com>
2022-11-15 18:06:06 +0800

    Readded to origin, VIC code of Get all products and ID

--- src/main/java/com/academy/assetsproject/controller/ProductController.java
+++ src/main/java/com/academy/assetsproject/controller/ProductController.java
@@ -2,10 +2,15 @@ package com.academy.assetsproject.controller;
 
 import com.academy.assetsproject.exception.RecordNotFoundException;
 import com.academy.assetsproject.models.Products;
-import com.academy.assetsproject.repository.ProductRepository;
 import com.academy.assetsproject.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/product")
@@ -13,6 +18,20 @@ public class ProductController {
 
     @Autowired
     private ProductService productService;
+
+    @GetMapping
+    public ResponseEntity<Page<Products>> getAllProducts(Pageable pageable){
+        Page<Products> products = productService.findAllProducts(pageable);
+        return new ResponseEntity<Page<Products>>(products, HttpStatus.OK);
+    }
+    @GetMapping("/{id}")
+    public List<Products> findProductById(@PathVariable Long id)throws RecordNotFoundException {
+        return productService.findByProductById(id);
+    }
+    @DeleteMapping("/{id}")
+    public void deleteProducts(@PathVariable Long id)throws RecordNotFoundException{
+        //productService.deleteProducts(id);
+    }
 
     @PostMapping
     public Products saveProduct(@RequestBody Products products){
